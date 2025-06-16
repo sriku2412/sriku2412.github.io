41f1f95529793fb3a46e84ee77090367866fc2f8
Sreekanth <srikanth.potlabathini@gmail.com>
2025-06-16 19:23:17 -0400

    footer update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
 import '../assets/styles/Footer.scss'
 
 function Footer() {
@@ -9,8 +12,11 @@ function Footer() {
       <div>
         <a href="https://github.com/sriku2412" target="_blank" rel="noreferrer"><GitHubIcon/></a>
         <a href="https://www.linkedin.com/in/potlabathini/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+        <a href="mailto:srikanth.potlabathini@gmail.com?subject=Hello%20Sreekanth%20Potlabathini" target="_blank" rel="noreferrer"><EmailOutlinedIcon/></a>
+        <a href="https://calendar.app.google/QSuqQv8Z7QAj1hvg9" target="_blank" rel="noreferrer"><CalendarMonthIcon/></a>
+        <a href="https://github.com/sriku2412/sriku2412/raw/main/Sreekanth-Resume-Jun-2025.pdf" target="_blank" rel="noreferrer"><FileDownloadIcon/></a>
       </div>
-      <p>Thanks to Yuji Sato for the template <a href="https://github.com/yujisatojr/react-portfolio-template" target="_blank" rel="noreferrer"></a>💜</p>
+      <p>Thanks to your visit!  <a href="https://github.com/yujisatojr/react-portfolio-template" target="_blank" rel="noreferrer"> Built with react</a>💜</p>
     </footer>
   );
 }
